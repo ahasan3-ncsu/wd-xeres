e94b40a3fae34c4e0a826d50ddbec83625f5723c
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2025-09-19 17:31:24 -0400

    Locate deposited ions

---
+++ bca/3d/deposited.py
@@ -0,0 +1,45 @@
+import sys
+import numpy as np
+from vispy import app, scene
+
+# feed a "deposited.output" file
+fileName = sys.argv[1]
+jar = np.loadtxt(fileName, delimiter=',')
+
+Z = jar[:, 1]
+pos_fin = jar[:, 2:5]
+
+canvas = scene.SceneCanvas(keys='interactive', show=True)
+view = canvas.central_widget.add_view()
+view.camera = 'turntable'
+
+custom_colors = {
+    39: (0.0, 1.0, 1.0, 1), # Y  = Cyan
+    42: (0.0, 0.0, 1.0, 1), # Mo = Blue
+    53: (0.0, 1.0, 0.0, 1), # I  = Green
+    54: (1.0, 1.0, 0.0, 1), # Xe = Yellow
+    92: (1.0, 0.0, 0.0, 1)  # U  = Red
+}
+z_colors = [custom_colors.get(z, (0.5, 0.5, 0.5, 1)) for z in Z]
+
+scatter = scene.visuals.Markers()
+scatter.set_data(pos_fin, edge_width=0, face_color=z_colors, size=5)
+view.add(scatter)
+
+sphere = scene.visuals.Sphere(
+    radius=1000,
+    method='latitude',
+    color=(0.8, 0.2, 0.2, 0.8),
+    parent=view.scene
+)
+
+cube = scene.visuals.Box(
+    10000, 10000, 10000,
+    color=(0.8, 0.8, 0.8, 0.2),
+    parent=view.scene
+)
+
+axis = scene.visuals.XYZAxis(parent=view.scene)
+
+if __name__ == '__main__':
+    app.run()
