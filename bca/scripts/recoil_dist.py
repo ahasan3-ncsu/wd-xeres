8121ae0a4a04d631a7bbc4a7daa7920250324447
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2025-09-21 01:32:44 -0400

    Plot recoil displacements vs energy

---
+++ bca/scripts/recoil_dist.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def calc(disp_file, to_plot):
+    E = [[], [], []]
+    D = [[], [], []]
+
+    with open(disp_file, 'r') as f:
+        for line in f:
+            tmp = line.split(',')
+            energ = float(tmp[2])
+            dr2 = [(float(tmp[6+i]) - float(tmp[3+i]))**2 for i in range(3)]
+
+            i = -1
+            match int(tmp[1]):
+                case 92:
+                    i = 0
+                case 42:
+                    i = 1
+                case 54:
+                    i = 2
+                case _:
+                    raise ValueError('Weird atomic number!')
+
+            E[i].append(energ)
+            D[i].append(sum(dr2)**0.5)
+
+    if int(to_plot):
+        plt.scatter(E[0], D[0],
+                    marker='s', s=5, color='red', label='U')
+        plt.scatter(E[1], D[1],
+                    marker='o', s=5, color='blue', label='Mo')
+        plt.scatter(E[2], D[2],
+                    marker='D', s=5, color='yellow', label='Xe')
+
+        plt.xscale('log')
+        plt.yscale('log')
+
+        plt.xlabel('Recoil energy (eV)')
+        plt.ylabel(r'Displacement ($\AA$)')
+
+        plt.legend(loc='lower right')
+        plt.savefig('/'.join(disp_file.split('/')[:-1]) + '/umoxe_dr.pdf')
+
+def main():
+    file_root = sys.argv[1]
+    disp_file = file_root + '_displacements.output'
+    to_plot = sys.argv[2]
+
+    calc(disp_file, to_plot)
+
+if __name__ == '__main__':
+    main()
--- bca/scripts/xe_calc.py
+++ bca/scripts/xe_calc.py
@@ -3,36 +3,46 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from toml_util import get_sphere_prop
 
-def calc(fileName, toPlot):
+def calc(disp_file, toml_file, to_plot):
     E = []
+    E_max = 0
+    E_min = 1e9
     R_ini = []
     R_fin = []
 
-    with open(fileName, 'r') as f:
+    with open(disp_file, 'r') as f:
         for line in f:
             if line[:6] == '134,54':
                 tmp = line.split(',')
-                E.append(float(tmp[2]))
+
+                energ = float(tmp[2])
+                E.append(energ)
+                if energ > E_max:
+                    E_max = energ
+                if energ < E_min:
+                    E_min = energ
+
                 r2 = float(tmp[3])**2 + float(tmp[4])**2 + float(tmp[5])**2
                 R_ini.append(r2**0.5)
                 r2 = float(tmp[6])**2 + float(tmp[7])**2 + float(tmp[8])**2
                 R_fin.append(r2**0.5)
 
-    sorted_tuple = sorted(zip(E, R_ini, R_fin))
-    E_sorted, R_ini_sorted, R_fin_sorted = zip(*sorted_tuple)
+    Rb = get_sphere_prop(toml_file)
+    delta = 10
 
     print(
-        ' Total Xe recoils: ', len(R_fin_sorted), '\n',
-        'Outside the sphere: ', sum(1 for x in R_fin_sorted if x > 40), '\n',
-        'Re-solved Xe: ', sum(1 for x in R_fin_sorted if x > 50)
+        ' Total Xe recoils: ', len(R_fin), '\n',
+        'Outside the sphere: ', sum(1 for x in R_fin if x > Rb), '\n',
+        'Re-solved Xe: ', sum(1 for x in R_fin if x > (Rb + delta))
     )
 
-    if int(toPlot):
+    if int(to_plot):
         up = []
         down = []
 
-        for e, i, f in zip(E_sorted, R_ini_sorted, R_fin_sorted):
+        for e, i, f in zip(E, R_ini, R_fin):
             if f > i:
                 up.append([e, i])
             else:
@@ -43,33 +53,36 @@ def calc(fileName, toPlot):
 
         # scatters
         plt.scatter(np_up[:, 0], np_up[:, 1],
-                    marker='^', s=5, color='orangered', label=r'$\mathbf{r_{i, ini}}$')
+                    marker='^', s=5, color='orangered',
+                    label=r'$\mathbf{r_{i, ini}}$')
         plt.scatter(np_down[:, 0], np_down[:, 1],
                     marker='v', s=5, color='orangered')
-        plt.scatter(E_sorted, R_fin_sorted,
-                    marker='s', s=5, color='seagreen', label=r'$\mathbf{r_{i, fin}}$')
+        plt.scatter(E, R_fin,
+                    marker='s', s=5, color='seagreen',
+                    label=r'$\mathbf{r_{i, fin}}$')
 
         # horizontal lines
-        plt.hlines(40, xmin=1, xmax=1000,
-                color='k', label=r'$R_b$')
-        plt.hlines(50, xmin=1, xmax=1000,
-                color='crimson', ls='--', label=r'$R_b + \delta$')
+        plt.hlines(Rb, xmin=0.9*E_min, xmax=1.1*E_max,
+                color='k', lw=1, label=r'$R_b$')
+        plt.hlines(Rb + delta, xmin=0.9*E_min, xmax=1.1*E_max,
+                color='crimson', ls='--', lw=1, label=r'$R_b + \delta$')
 
         plt.xscale('log')
         plt.yscale('log')
 
         plt.xlabel('Xe recoil energy (eV)')
-        plt.ylabel(r'Distance from the bubble center ($\AA$)')
+        plt.ylabel(r'Distance from bubble center ($\AA$)')
 
         plt.legend(loc='lower right')
-        plt.savefig('/'.join(fileName.split('/')[:-1]) + '/xe_dist.pdf')
+        plt.savefig('/'.join(disp_file.split('/')[:-1]) + '/xe_res.pdf')
 
 def main():
-    args = sys.argv[1:]
-    file = args[0]
-    to_plot = args[1]
+    file_root = sys.argv[1]
+    disp_file = file_root + '_displacements.output'
+    toml_file = file_root + '.toml'
+    to_plot = sys.argv[2]
 
-    calc(file, to_plot)
+    calc(disp_file, toml_file, to_plot)
 
 if __name__ == '__main__':
     main()
