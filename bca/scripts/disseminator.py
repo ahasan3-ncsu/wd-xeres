6796b5354802f0e986184ef6ff2e80e31cc44776
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2025-10-15 17:32:40 -0400

    Generate input.toml files
    And move them to appropriate directories

--- bca/scripts/disseminator.py
+++ bca/scripts/disseminator.py
@@ -1,16 +1,66 @@
 import os
 import shutil
+from input_gen import gen_toml
 
-p_eq = {1: 0.00905, 2: 0.00908, 4: 0.01009, 8: 0.00868, 16: 0.00662, 32: 0.00479, 64: 0.00327, 128: 0.00209}
+# equilibrium number density for a radius (subject to change)
+n_eq = {
+    1: 0.00905,
+    2: 0.00908,
+    4: 0.01009,
+    8: 0.00868,
+    16: 0.00662,
+    32: 0.00479,
+    64: 0.00327,
+    128: 0.00209
+}
 
+# energy discretization
 Y_en = [102, 80, 60, 40, 20, 10, 5, 2, 1, 0.5, 0.1]
 I_en = [75, 60, 40, 20, 10, 5, 2, 1, 0.5, 0.1]
 
+# only intergranular for now
 radii = [64, 128]
 
+arg_dict = {'filename': 'ballbox.toml'}
+
 for rad in radii:
+    arg_dict['sphere'] = [rad * 10, n_eq[rad]]
+
     os.makedirs(f'{rad}nm', exist_ok=True)
+
     for y_e in Y_en:
+        # check if the directory exists already
+        # continue to the next energy if it does
+        # this will enable getting incremental discrete values
+
+        arg_dict['yttrium'] = [1000, y_e * 1e6, 0.0]
+        arg_dict['iodine'] = [0, 0.0, 0.0]
+
+        # make input.toml file
+        input_file = gen_toml(arg_dict)
+
+
+        # move input file
         os.makedirs(f'{rad}nm/Y_{y_e}MeV', exist_ok=True)
+        shutil.move(input_file, f'{rad}nm/Y_{y_e}MeV/')
+
+        # use a runner.py-like script to do sims (do 5 runs)
+        # run agg.py-like script to check for convergence (check error)
+
     for i_e in I_en:
+        # check if the directory exists already
+        # continue to the next energy if it does
+        # this will enable getting incremental discrete values
+
+        arg_dict['yttrium'] = [0, 0.0, 0.0]
+        arg_dict['iodine'] = [1000, i_e * 1e6, 0.0]
+
+        # make input.toml file
+        input_file = gen_toml(arg_dict)
+
+        # move input file
         os.makedirs(f'{rad}nm/I_{i_e}MeV', exist_ok=True)
+        shutil.move(input_file, f'{rad}nm/I_{i_e}MeV/')
+
+        # use a runner.py-like script to do sims (do 5 runs)
+        # run agg.py-like script to check for convergence (check error)
--- bca/scripts/input_gen.py
+++ bca/scripts/input_gen.py
@@ -1,19 +1,98 @@
-import sys
 import tomlkit
+import argparse
+from si_pref import num_to_si
 
-def read_toml(toml_file):
-    with open('ballbox.toml', 'r') as f:
-        data = tomlkit.load(f)
+def gen_toml(arg_dict):
+    ### READ
+    with open(arg_dict['filename'], 'r') as f:
+        toml_dict = tomlkit.load(f)
 
-    dir = data['particle_parameters']['dir'][0]
-    print(dir[0]**2 + dir[1]**2)
-    print(data['geometry_input']['sphere_radius'])
-    print(data['geometry_input']['cuboid_corners'])
+    ### GEOMETRY
+    delta = 1e3    # 0.1 micron separation
+    radius = float(arg_dict['sphere'][0])
+    density = float(arg_dict['sphere'][1])
+    box_half = radius + delta + 1.0
+
+    toml_dict['geometry_input']['sphere_radius'] = radius
+    toml_dict['geometry_input']['sphere_densities'] = [0.0, 0.0, density]
+    toml_dict['geometry_input']['cuboid_corners'] = [
+        [-box_half, -box_half, -box_half], [box_half, box_half, box_half]
+    ]
+
+    ### YTTRIUM
+    y_num = int(arg_dict['yttrium'][0])
+    toml_dict['particle_parameters']['N'][0] = y_num
+
+    y_en = float(arg_dict['yttrium'][1])
+    toml_dict['particle_parameters']['E'][0] = y_en
+
+    y_l = float(arg_dict['yttrium'][2])
+    toml_dict['particle_parameters']['pos'][0] = [- radius - delta, y_l / 2**0.5, y_l / 2**0.5]
+
+    y_str = ''
+    if y_num:
+        y_str = f'_Y_{num_to_si(y_en)}eV'
+        if y_l:
+            y_str += f'_l{y_l/10:.0f}nm'
+
+    ### IODINE
+    i_num = int(arg_dict['iodine'][0])
+    toml_dict['particle_parameters']['N'][1] = i_num
+
+    i_en = float(arg_dict['iodine'][1])
+    toml_dict['particle_parameters']['E'][1] = i_en
+
+    i_l = float(arg_dict['iodine'][2])
+    toml_dict['particle_parameters']['pos'][1] = [- radius - delta, i_l / 2**0.5, i_l / 2**0.5]
+
+    i_str = ''
+    if i_num:
+        i_str = f'_I_{num_to_si(i_en)}eV'
+        if i_l:
+            i_str += f'_l{i_l/10:.0f}nm'
+
+    ### WRITE
+    file_root = arg_dict['filename'].split('.')[0]
+    output_name = file_root + f'_{radius/10:.0f}nm' + y_str + i_str
+    toml_dict['options']['name'] = output_name
+
+    output_toml = output_name + '.toml'
+    with open(output_toml, 'w') as f:
+        tomlkit.dump(toml_dict, f)
+
+    return output_toml
 
 def main():
-    toml_file = sys.argv[1]
+    parser = argparse.ArgumentParser(
+        prog='ballbox replicator',
+        description='Generate a new input.toml based on ballbox',
+        epilog='see you space cowboy'
+    )
 
-    read_toml(toml_file)
+    parser.add_argument('filename', help='original ballbox.toml file')
+    parser.add_argument(
+        '-Y', '--yttrium',
+        nargs=3,
+        metavar=('N', 'E', 'l'),
+        help='Y number, energy and off-centered distance',
+    )
+    parser.add_argument(
+        '-I', '--iodine',
+        nargs=3,
+        metavar=('N', 'E', 'l'),
+        help='I number, energy and off-centered distance'
+    )
+    parser.add_argument(
+        '-S', '--sphere',
+        nargs=2,
+        metavar=('R', 'rho'),
+        help='sphere radius and number density'
+    )
+
+    args = parser.parse_args()
+
+    print(vars(args))
+    print(gen_toml(vars(args)))
 
 if __name__ == '__main__':
     main()
