e451bd36e086b24732a2bfb5f7ac23f75ce66bc5
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2025-10-24 23:08:37 -0400

    Try some fits to the offcent data

--- bca/offcent/plc.py
+++ bca/offcent/plc.py
@@ -24,58 +24,58 @@ def plotter(data_dir, mode=1):
 
         chi = [c / chi[0] for c in chi]
 
-        m = chi[2]
-
         Rb = int(jname.split('_')[0][:-2]) * 10
+
+        if mode == 1:
+            L = [l / Rb for l in L]
+        elif mode == 2:
+            L = [l - Rb for l in L]
+        elif mode == 3:
+            L = [l / L[-1] for l in L]
 
         mark = next(markers)
         tint = next(tints)
         lsty = next(lstyles)
 
-        if mode == 1:
-            L = [l / Rb for l in L]
-
-            # anchor right end to chi(E, R_b)
-            # xa = np.linspace(0, 1, 100)
-            # ya = [1 + (m-1) * x for x in xa]
-            # plt.plot(
-            #     xa, ya,
-            #     color=plt.cm.jet(tint),
-            #     ls=lsty
-            # )
-        elif mode == 2:
-            L = [l - Rb for l in L]
-
-            # anchor left end to chi(E, R_b)
-            # xb = np.linspace(0, 1000, 100)
-            # yb = [m * np.exp(-x/200) for x in xb]
-            # plt.plot(
-            #     xb, yb,
-            #     color=plt.cm.jet(tint),
-            #     ls=lsty
-            # )
-
-        plt.plot(
-            L, chi,
-            marker=mark,
-            color=plt.cm.jet(tint),
-            ls=lsty,
-            label=f'{jname[:-5]}'
-        )
-
-        # plt.scatter(
-        #     L, chi,
-        #     marker=mark,
-        #     color=plt.cm.jet(tint),
-        # )
-        #
         # plt.plot(
-        #     [], [],
+        #     L, chi,
         #     marker=mark,
         #     color=plt.cm.jet(tint),
         #     ls=lsty,
         #     label=f'{jname[:-5]}'
         # )
+
+        def fit(x, x1, y1, x2, y2):
+            if x < x1:
+                return 1 + (y1 - 1) / x1 * x
+            else:
+                c = (x2 - x1) / np.log(y1 / y2)
+                return y1 * np.exp(-(x - x1) / c)
+        xc = np.linspace(0, L[-1], 100)
+        yc = [
+            fit(x, L[4], chi[4], L[-4], chi[-4])
+            for x in xc
+        ]
+
+        plt.plot(
+            xc, yc,
+            color=plt.cm.jet(tint),
+            ls=lsty
+        )
+
+        plt.scatter(
+            L, chi,
+            marker=mark,
+            color=plt.cm.jet(tint),
+        )
+
+        plt.plot(
+            [], [],
+            marker=mark,
+            color=plt.cm.jet(tint),
+            ls=lsty,
+            label=f'{jname[:-5]}'
+        )
 
     if mode == 1:
         plt.xlim([0, 1])
@@ -83,6 +83,10 @@ def plotter(data_dir, mode=1):
     elif mode == 2:
         plt.xlim([0, 1000])
         plt.xlabel(r'$\ell$ - $R_b$')
+    elif mode == 3:
+        plt.xlabel(r'$\ell$ / ($R_b + \delta$)')
+    elif mode == 4:
+        plt.xlabel(r'$\ell$')
 
     plt.ylabel(r'$\chi(E, \ell)$ / $\chi(E, 0)$')
 
@@ -95,6 +99,8 @@ def main():
 
     plotter(data_dir, 1)
     plotter(data_dir, 2)
+    plotter(data_dir, 3)
+    plotter(data_dir, 4)
 
 if __name__ == '__main__':
     main()
